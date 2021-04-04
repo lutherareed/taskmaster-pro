93e3c096f115b56c3a9a2ab727f48ec02299b518
Luther Reed <lutherareed@gmail.com>
2021-04-04 01:10:06 -0500

    added drag-drop functionality and enabled trash drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -173,5 +173,62 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+// add drag-and-drop within a list or between lists
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // new array to store the resorted task data in
+    var tempArr = [];
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this).find("p").text().trim();
+      var date = $(this).find("span").text().trim();
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+    // trim down the list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    // update correct array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event,ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+})
+
 // load tasks for the first time
 loadTasks();
